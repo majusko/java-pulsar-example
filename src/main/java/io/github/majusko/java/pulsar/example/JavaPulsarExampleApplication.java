35aaba6be3281a808fe669a7112725b5ae0729f9
majusko <mariokapustaa@gmail.com>
2020-01-01 16:07:45 +0000

    Init project.

---
+++ src/main/java/io/github/majusko/java/pulsar/example/JavaPulsarExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.majusko.java.pulsar.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPulsarExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPulsarExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/majusko/java/pulsar/example/JavaPulsarExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.majusko.java.pulsar.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPulsarExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
