497d31d8e3ed9499e417d2be38b9b43f69cbe3e7
majusko <mariokapustaa@gmail.com>
2020-01-03 19:07:51 +0000

    Add dependency.

---
+++ src/main/java/io/github/majusko/java/pulsar/example/configuration/ProducerConfiguration.java
@@ -0,0 +1,8 @@
+package io.github.majusko.java.pulsar.example.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProducerConfiguration {
+
+}
