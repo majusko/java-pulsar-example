a024245ea1e64a55f7c5714522f9178c353415e7
majusko <mariokapustaa@gmail.com>
2020-01-05 17:23:01 +0000

    Implemented configuration of consumer and producer.

--- src/main/java/io/github/majusko/java/pulsar/example/configuration/ProducerConfiguration.java
+++ src/main/java/io/github/majusko/java/pulsar/example/configuration/ProducerConfiguration.java
@@ -1,8 +1,16 @@
 package io.github.majusko.java.pulsar.example.configuration;
 
+import io.github.majusko.pulsar.producer.ProducerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProducerConfiguration {
+
+    @Bean
+    public ProducerFactory producerFactory() {
+        return new ProducerFactory()
+            .addProducer(Topics.STRING, String.class);
+    }
 
 }
