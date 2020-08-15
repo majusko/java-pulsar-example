895c3231a492ae90b681fc48fdef3815be0ac894
Mario Kapusta <mariokapustaa@gmail.com>
2020-08-15 12:55:47 +0200

    Added support for tests.

--- src/test/java/io/github/majusko/java/pulsar/example/JavaPulsarExampleApplicationTests.java
+++ src/test/java/io/github/majusko/java/pulsar/example/JavaPulsarExampleApplicationTests.java
@@ -2,12 +2,26 @@ package io.github.majusko.java.pulsar.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PulsarContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class JavaPulsarExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PulsarContainer pulsarContainer = new PulsarContainer();
 
+    @DynamicPropertySource
+    static void propertySettings(DynamicPropertyRegistry registry) {
+        registry.add("pulsar.serviceUrl", pulsarContainer::getPulsarBrokerUrl);
+    }
+
+    @Test
+    void contextLoads() {
+        //TODO
+    }
 }
