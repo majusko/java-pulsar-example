2e52248be229b7ea82ef1e9739eff0d6199ea7f3
Mario Kapusta <mariokapustaa@gmail.com>
2021-10-28 21:59:21 +0300

    Refactored consumer service.

--- src/main/java/io/github/majusko/java/pulsar/example/consumer/ConsumerService.java
+++ src/main/java/io/github/majusko/java/pulsar/example/consumer/ConsumerService.java
@@ -3,7 +3,6 @@ package io.github.majusko.java.pulsar.example.consumer;
 import io.github.majusko.java.pulsar.example.configuration.Topics;
 import io.github.majusko.java.pulsar.example.data.MyMsg;
 import io.github.majusko.pulsar.annotation.PulsarConsumer;
-import io.github.majusko.pulsar.constant.Serialization;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -13,14 +12,14 @@ public class ConsumerService {
     public AtomicBoolean stringReceived = new AtomicBoolean(false);
     public AtomicBoolean classReceived = new AtomicBoolean(false);
 
-    @PulsarConsumer(topic= Topics.STRING, clazz=String.class)
-    public void consumeString(String message){
+    @PulsarConsumer(topic = Topics.STRING, clazz = String.class)
+    public void consumeString(String message) {
         System.out.println(message);
         stringReceived.set(true);
     }
 
-    @PulsarConsumer(topic= Topics.CLASS, clazz = MyMsg.class, serialization = Serialization.JSON)
-    public void consumeClass(MyMsg message){
+    @PulsarConsumer(topic = Topics.CLASS, clazz = MyMsg.class)
+    public void consumeClass(MyMsg message) {
         System.out.println(message.getData());
         classReceived.set(true);
     }
