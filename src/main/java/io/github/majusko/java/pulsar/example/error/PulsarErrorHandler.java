1a18ac7def0f94d7de72d3b4694b3788bf6a1bc2
majusko <mariokapustaa@gmail.com>
2023-01-06 12:48:22 +0100

    Adding dependency await on library bean.

--- src/main/java/io/github/majusko/java/pulsar/example/error/PulsarErrorHandler.java
+++ src/main/java/io/github/majusko/java/pulsar/example/error/PulsarErrorHandler.java
@@ -2,20 +2,22 @@ package io.github.majusko.java.pulsar.example.error;
 
 import io.github.majusko.pulsar.consumer.ConsumerAggregator;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@DependsOn({"consumerAggregator"})
 public class PulsarErrorHandler {
 
-    private final ConsumerAggregator aggregator;
+    private final ConsumerAggregator consumerAggregator;
 
-    public PulsarErrorHandler(ConsumerAggregator aggregator) {
-        this.aggregator = aggregator;
+    public PulsarErrorHandler(ConsumerAggregator consumerAggregator) {
+        this.consumerAggregator = consumerAggregator;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void pulsarErrorHandler() {
-        aggregator.onError(failedMessage -> failedMessage.getException().printStackTrace());
+        consumerAggregator.onError(failedMessage -> failedMessage.getException().printStackTrace());
     }
 }
